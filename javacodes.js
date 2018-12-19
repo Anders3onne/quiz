cf51271a76a772a7d341259975e2158a2100c369
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2018-12-19 10:01:47 +0200

    added styles css

--- javacodes.js
+++ javacodes.js
@@ -110,4 +110,9 @@ function showResults(questions, quizContainer, resultsContainer){
 submitButton.onclick = function(){
 	showResults(questions, quizContainer, resultsContainer);
 }
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
 
