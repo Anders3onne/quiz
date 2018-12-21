f4bc6962e813ed673041a1039ce35d97559fbc9e
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2018-12-21 09:20:16 +0200

    add insertions

--- javacodes.js
+++ javacodes.js
@@ -1,43 +1,52 @@
 function check(){
 
-    var q1 = document.quiz.q1.value;
-    var q2 = document.quiz.q2.value;
-    var q3 = document.quiz.q3.value;
-    var q4 = document.quiz.q4.value;
+    var A1 = document.quiz.A1.value;
+    var A2 = document.quiz.A2.value;
+    var A3 = document.quiz.A3.value;
+    var A4 = document.quiz.A4.value;
+    var A5 = document.quiz.A5.value;
+    var A6 = document.quiz.A6.value;
+    var A7 = document.quiz.A7.value;
+    var A8 = document.quiz.A8.value;
+    var A9 = document.quiz.A9.value;
+    var A10 = document.quiz.A10.value;
+    var A11 = document.quiz.A11.value;
     var count=0;
-  
-        if (q1 == "c") {        count+=25;
-        }
-        if (q2 == "c"){
+
+        if (A1 == "b") {        
           count+=25;
         }
-        if (q3 == "b"){
+        if (A2 == "c"){
           count+=25;
         }
-        if (q4 == "c"){
+        if (A3 == "a"){
           count+=25;
         }
-        if (q4 == "c"){
+        if (A4 == "c"){
+          count+=25;
+        }
+        if (A4 == "c"){
             count+=25;
           }
-          if (q1 == "c") {        count+=25;
+        if (A5 == "c") {    
+                count+=25;
           }
-          if (q2 == "c"){
+        if (A6 == "c"){
             count+=25;
           }
-          if (q3 == "b"){
+        if (A7 == "b"){
             count+=25;
           }
-          if (q4 == "c"){
+        if (A8== "c"){
             count+=25;
           }
-          if (q4 == "c"){
+        if (A9 == "c"){
               count+=25;
             }
-            if (q4 == "c"){
+        if (q10 == "c"){
                 count+=25;
               }
-              if (q4 == "c"){
+        if (q11 == "c"){
                   count+=25;
                 }
                 
